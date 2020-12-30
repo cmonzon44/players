f37c72b4d8c6abe0d34ae0c4d4a5a738171e43d9
cmonzon44 <cmonzon44@gmail.com>
2020-12-30 02:12:14 -0300

    spinner ngx bootstrap and primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
   title = 'players';
+  items: MenuItem[];
+
+  constructor(private _primeNGConfig: PrimeNGConfig) {
+
+  }
+
+  ngOnInit() {
+    this._primeNGConfig.ripple = true;
+
+    this.items = [
+      {
+        label: 'Sports',
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: 'NBA',
+            icon: 'pi pi-fw pi-plus',
+            items: [
+              {
+                label: 'LA Lakers',
+                icon: 'pi pi-fw pi-plus'
+              },
+              {
+                label: 'LA Clippers',
+                icon: 'pi pi-fw pi-plus'
+              },
+              {
+                label: 'Toronto',
+                icon: 'pi pi-fw pi-plus'
+              },
+              {
+                label: 'Miami Heat',
+                icon: 'pi pi-fw pi-plus'
+              }
+            ]
+          }
+        ]
+      },
+      
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AtomSpinnerModule } from 'angular-epic-spinners'
+
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AtomSpinnerModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
